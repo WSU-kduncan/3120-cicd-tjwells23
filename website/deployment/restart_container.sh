019a6a9debba3f8b7607fc5e9e0d26bb02ddbecf
tjwells23 <tjwells023@gmail.com>
2023-04-12 21:27:51 -0400

    saved changes

--- website/deployment/restart_container.sh
+++ website/deployment/restart_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-sudo docker pull my-website:latest
+sudo docker pull tjwells023/tjwellsdocker:latest
 sudo docker stop $(sudo docker ps -q --filter ancestor=my-website)
 sudo docker rm $(sudo docker ps -a -q --filter ancestor=my-website)
-sudo docker run -d -p 80:80 my-website:latest
+sudo docker run -d -p 8080 tjwells023/tjwellsdocker:latest
