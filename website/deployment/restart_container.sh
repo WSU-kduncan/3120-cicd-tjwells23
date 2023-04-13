d84b51ddd4ea51d2567b410293a41ff688d66be8
tjwells23 <tjwells023@gmail.com>
2023-04-12 21:46:39 -0400

    changes to ymls and json

--- website/deployment/restart_container.sh
+++ website/deployment/restart_container.sh
@@ -4,4 +4,4 @@
 sudo docker pull tjwells023/tjwellsdocker:latest
 sudo docker stop $(sudo docker ps -q --filter ancestor=my-website)
 sudo docker rm $(sudo docker ps -a -q --filter ancestor=my-website)
-sudo docker run -d -p 8080 tjwells023/tjwellsdocker:latest
+sudo docker run tjwells023/tjwellsdocker:latest
--- website/restart_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-sudo docker pull my-website:latest
-sudo docker stop $(sudo docker ps -q --filter ancestor=my-website)
-sudo docker rm $(sudo docker ps -a -q --filter ancestor=my-website)
-sudo docker run -d -p 80:80 my-website:latest
